4d86b66de27d0ce9361d41fefb477b8d0341e8f5
Trent Buckley <78893529+BoredManCodes@users.noreply.github.com>
2021-09-10 14:53:44 +1000

    Woot Initial Commit
    And I didn't even include the bot token in the .env. I'm learnding: https://i.redd.it/e4429p82liq01.jpg

---
+++ main.py
@@ -0,0 +1,17 @@
+##################################################
+#                                                #
+#             DeepL Translate Bot                #
+#                                                #
+#       Hacked together by BoredManPlays         #
+#                                                #
+##################################################
+
+
+from decouple import config
+
+
+# let's learn how to read from .env files
+TOKEN = config("TOKEN")
+print(TOKEN)
+
+
